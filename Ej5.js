38774f7cddf0dd937829403da16264ae640ec4f1
nataliayepes11 <nataliayepes11@gmail.com>
2026-01-07 15:00:34 -0300

    FOREACH

--- Ej5.js
+++ Ej5.js
@@ -2,3 +2,9 @@
 Tienes una lista de frutas ['manzana', 'banana', 'cereza']. 
 Tu tarea es recorrer la lista y mostrar el nombre de cada fruta en la consola, una por una.
 */
+
+let frutas = [`manzana`, `banana`, `cereza`];
+
+frutas.forEach(function(valor){
+    console.log(valor);
+});
