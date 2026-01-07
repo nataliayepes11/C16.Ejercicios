94f673420fe67d38c886e07d7181695b71426093
nataliayepes11 <nataliayepes11@gmail.com>
2026-01-07 14:54:43 -0300

    REDUCE

--- Ej4.js
+++ Ej4.js
@@ -3,3 +3,10 @@ Dada una lista de números [1, 2, 3, 4], necesitamos calcular el resultado de su
 Es decir, deberías obtener 1 + 2 + 3 + 4. Al fi nalizar, imprime el total en la consola.
 */
 
+let lista = [1, 2, 3, 4];
+
+let suma = lista.reduce(function(acumulador, numero){
+    return acumulador + numero;
+});
+
+console.log("Los números del array suman: " + suma);
