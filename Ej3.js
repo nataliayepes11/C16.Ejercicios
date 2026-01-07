d4279997bf7463b09fde02e8652a103d4ddc3890
nataliayepes11 <nataliayepes11@gmail.com>
2026-01-07 14:49:12 -0300

    FIND

--- Ej3.js
+++ Ej3.js
@@ -4,3 +4,11 @@ Queremos encontrar el primer número de la lista que sea mayor a 10.
 Recuerda que el resultado debe ser únicamente el primer número que cumpla esta condición, 
 no todos los números que lo hagan.
 */
+
+let lista = [5, 8, 12, 20, 3];
+
+let primeroMayorDiez = lista.find(function (numero){
+    return numero > 10;
+});
+
+console.log("El primer número mayor a 10 es: " + primeroMayorDiez);
