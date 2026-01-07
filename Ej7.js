ad8443b4cbda6291ccfcefc995b428e31bd6e0a8
nataliayepes11 <nataliayepes11@gmail.com>
2026-01-07 15:18:11 -0300

    FILTER (COMIENZAN CON A)

--- Ej7.js
+++ Ej7.js
@@ -4,3 +4,11 @@ queremos quedarnos solo con los nombres que comiencen con la letra 'A'.
 Por ejemplo, el nombre 'Luis' no debería estar en el resultado porque empieza con 'L'.
 */
 
+let nombres = [`Ana`, `Luis`, `Andrea`, `María`];
+
+let comienzanConA = nombres.filter(function(nombre){
+    return nombre[0] === `A` ;
+
+});
+
+console.log("Los nombres que comienzan con A son: " + comienzanConA);
