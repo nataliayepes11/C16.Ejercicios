2a7c8f2a959147b64cf5d181299e959f74a83c0a
nataliayepes11 <nataliayepes11@gmail.com>
2026-01-08 11:20:46 -0300

    REDUCE (CANTIDAD TOTAL DE LETRAS EN VARIAS PALABRAS)

--- Ej9.js
+++ Ej9.js
@@ -3,3 +3,11 @@ Imagina que tienes una lista de palabras ['sol', 'luna', 'estrella'].
 Queremos saber cuántas letras hay en total entre todas las palabras. 
 Por ejemplo, 'sol' tiene 3 letras, 'luna' tiene 4, y 'estrella' tiene 8. La suma total debería ser 15.
 */ 
+
+let universo = [`sol`, `luna`, `estrella`];
+
+let cantidadDeLetras = universo.reduce(function(acumulador, string){
+    return acumulador + string.length;
+}, 0); // ponemnos CERO para que el acumulador inicie en 0.
+
+console.log("El array tiene " + (cantidadDeLetras) + " letras");
