e806fba257906f4d398b6950b021a5c7669d9291
nataliayepes11 <nataliayepes11@gmail.com>
2026-01-08 11:08:55 -0300

    FIND (NOMBRE)

--- Ej8.js
+++ Ej8.js
@@ -1,4 +1,12 @@
-/* Ejercicio 8: Busca un nombre específi co
+/* Ejercicio 8: Busca un nombre específico
 Tienes una lista de nombres ['Carlos', 'Daniel', 'Laura', 'Ana']. 
 Queremos saber si el nombre 'Laura' está presente en la lista y, si lo está, deberías devolverlo.
 */
+
+let nombres = [`Carlos`, `Daniel`, `Laura`, `Ana`];
+
+let laura = nombres.find(function(string){
+    return string === "Laura";
+
+});
+console.log(laura);
